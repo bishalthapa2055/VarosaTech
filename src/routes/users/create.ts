2d2235377d6ee55f01c5886c2c5edc2ae1929baa
Bishal Thapa <bishalthapab989@gmail.com>
2023-02-09 19:21:37 +0545

    addition of validatiion

--- src/routes/users/create.ts
+++ src/routes/users/create.ts
@@ -1,8 +1,27 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createUsersHandler } from "../../controllers/create";
+import { validateRequest } from "../../middlewares/validate-request";
 
 const router = Router();
 
-router.post("/", createUsersHandler);
+router.post(
+  "/",
+
+  [
+    body("name")
+      .isLength({ min: 5 })
+      .withMessage("Name must be greater than 3 character"),
+    body("address").withMessage("Address is required Field"),
+    body("dateofbirth").withMessage("dateofbirth is required Field"),
+    body("nationality").withMessage("nationality is required Field"),
+    body("educationbackground").withMessage(
+      "educationbackground is required Field"
+    ),
+    body("gender").withMessage("gender is required Field"),
+  ],
+  validateRequest,
+  createUsersHandler
+);
 
 export { router as createUsersHandlerRouter };
